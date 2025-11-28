77ccb99327c5abaa3283430507e86616047ae15c
Avika Gupta <123083752+avikagupta03@users.noreply.github.com>
2025-11-28 16:44:11 +0530

    Add files via upload

--- android/app/src/main/kotlin/com/example/collabverse/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/collabverse/MainActivity.kt
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 package com.example.collabverse
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
+=======
+package com.example.collabverse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
+>>>>>>> f1d606d (second upload)
